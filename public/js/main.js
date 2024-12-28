fdd298519a4f0d0ea5afe6076fd90455547148f5
Andrei B <working.web31@gmail.com>
2024-12-28 16:46:23 +0200

    Make adaptive, add content and interactive

--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,45 @@ document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("header");
     const menuToggle = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
+    const menuItems = document.querySelectorAll('.header-menu__link, .footer-menu__link, *[href="#order"]');
     const body = document.body;
     const stickyOffset = header.offsetTop;
-  
+
     //Sticky Header
     window.addEventListener("scroll", () => {
-      if (window.scrollY > stickyOffset) {
-        header.classList.add("sticky");
-      } else {
-        header.classList.remove("sticky");
-      }
+        if (window.scrollY > stickyOffset) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
     });
 
     //Toggle Mobile Menu
-    menuToggle.addEventListener('click', () => {
-      const isMenuOpen = mobileMenu.classList.toggle('header-menu-mobile--show');
-      body.classList.toggle('no-scroll', isMenuOpen);
+    function toggleMenu() {
+        const isMenuOpen = mobileMenu.classList.toggle('header-menu-mobile--show');
+        body.classList.toggle('no-scroll', isMenuOpen);
+    }
+
+    menuToggle.addEventListener('click', toggleMenu);
+
+    //Close Mobile Menu on click
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            mobileMenu.classList.remove('header-menu-mobile--show');
+            body.classList.remove('no-scroll');
+        });
+    });
+
+    //Scroll to section
+    menuItems.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').slice(1);
+            const targetSection = document.getElementById(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
 });
